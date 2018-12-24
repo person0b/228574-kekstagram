6efbc8498f25420ba832a6fac59cbab8eea7e7d3
person0b <binaryperson@yandex.ru>
2018-12-24 12:21:49 +0300

    Fix D18

--- js/utils.js
+++ js/utils.js
@@ -37,13 +37,9 @@
     },
 
     mixArray: function (arr) {
-      for (var i = arr.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-      }
-      return arr;
+      return arr.sort(function () {
+        return 0.5 - Math.random();
+      });
     }
   };
 })();
